14d38a341c1081405bd373ac9d37d7d0c38758ae
m.onohara <m.onohara@micro-wave.net>
2020-10-04 17:47:11 +0900

    Specify hashFunction

--- lib/builder.ts
+++ lib/builder.ts
@@ -1,5 +1,5 @@
 import * as webpack from "webpack";
-import {Compiler, Configuration} from "webpack";
+import { Compiler, Configuration } from "webpack";
 
 /**
  * Builder options
@@ -28,19 +28,20 @@ export class Builder {
   private readonly webpack: Compiler;
 
   constructor(private readonly options: BuilderOptions) {
-      this.webpack = webpack(require(this.options.config) as Configuration)
-      this.webpack.options.entry = this.options.entry
-      this.webpack.options.output = {
-        libraryTarget: "commonjs",
-        path: this.options.output
-      }
+    this.webpack = webpack(require(this.options.config) as Configuration);
+    this.webpack.options.entry = this.options.entry;
+    this.webpack.options.output = {
+      libraryTarget: "commonjs",
+      path: this.options.output,
+      hashFunction: "md4",
+    };
   }
 
   public build(): void {
-    this.webpack.run((err : Error) => {
-      if(err) {
-        throw err
+    this.webpack.run((err: Error) => {
+      if (err) {
+        throw err;
       }
-    })
+    });
   }
 }
